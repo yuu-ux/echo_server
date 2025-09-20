de1fc198db5973a8a0b693a704c5f87c76ece71f
yuu-ux <yuugaehara@gmail.com>
2025-09-20 20:23:05 +0900

    add simple echo server

---
+++ c_echo/simple/main.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+
+int main(void) {
+    int sfd, cfd;
+
+    printf("hello\n");
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        fprintf(stderr, "failed to create a socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in my_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(8124),
+        .sin_addr = {
+            .s_addr = 0,
+        },
+    };
+
+    if (bind(sfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1) {
+        fprintf(stderr, "failed to bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sfd, 100) == -1) {
+        fprintf(stderr, "failed to listen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in peer_addr;
+    socklen_t peer_addr_size = sizeof(peer_addr);
+
+    while (1) {
+        cfd = accept(sfd, (struct sockaddr*) &peer_addr, &peer_addr_size);
+        if (cfd == -1) {
+            fprintf(stderr, "failed to accept");
+            exit(EXIT_FAILURE);
+        }
+        printf("accepted\n");
+
+        ssize_t len;
+        uint8_t buf[1024];
+        while ((len = read(cfd, buf, 1024)) != 0) {
+            ssize_t written = 0;
+            while (written < len) {
+                ssize_t ret = write(cfd, buf+written, len-written);
+                if (ret == -1) {
+                    fprintf(stderr, "failed to write\n");
+                    exit(EXIT_FAILURE);
+                }
+                written += ret;
+            }
+        }
+        printf("closed\n");
+    }
+    printf("%d ok\n", cfd);
+    return 0;
+}
